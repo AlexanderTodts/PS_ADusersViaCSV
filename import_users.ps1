9d7c15f344fd9019fc33d28c310a95cad6b331d2
AlexanderTodts <33911141+AlexanderTodts@users.noreply.github.com>
2018-01-08 19:43:10 +0100

    Update import_users.ps1

--- import_users.ps1
+++ import_users.ps1
@@ -1,5 +1,11 @@
-﻿
-$Users = Import-Csv -Path "C:\scripts\import_users.csv"            
+# PowerShell scriptje om nieuwe users aan te maken in ADUC
+# Users staan opgelijst in bijhorend csv-bestand (import_users.csv)
+# CSV structuur: Firstname,Lastname,Maildomain,SAM,Password
+
+# Alle users opgesomd in de CSV inlezen in de $Users variabele
+$Users = Import-Csv -Path "C:\scripts\import_users.csv"       
+
+# Voor elke entry(user) in de array $Users de opgegeven waarden invullen in ADUC
 foreach ($User in $Users)            
 {            
     $Displayname = $User.'Firstname' + " " + $User.'Lastname'            
@@ -8,5 +14,5 @@ foreach ($User in $Users)
     $SAM = $User.'SAM'            
     $UPN = $User.'Firstname' + "." + $User.'Lastname' + "@" + $User.'Maildomain'               
     $Password = $User.'Password'            
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false –PasswordNeverExpires $true            
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false            
 }
