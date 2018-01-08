1ee1a709185510b2954fd4c975f3eec5c66ce01e
AlexanderTodts <33911141+AlexanderTodts@users.noreply.github.com>
2018-01-08 19:31:52 +0100

    Add files via upload

---
+++ import_users - Copy.ps1
@@ -0,0 +1,12 @@
+﻿
+$Users = Import-Csv -Path "C:\scripts\import_users.csv"            
+foreach ($User in $Users)            
+{            
+    $Displayname = $User.'Firstname' + " " + $User.'Lastname'            
+    $UserFirstname = $User.'Firstname'            
+    $UserLastname = $User.'Lastname'                    
+    $SAM = $User.'SAM'            
+    $UPN = $User.'Firstname' + "." + $User.'Lastname' + "@" + $User.'Maildomain'               
+    $Password = $User.'Password'            
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false –PasswordNeverExpires $true            
+}
